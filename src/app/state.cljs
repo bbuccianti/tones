cb8ca9fbac02d0c5decb8b1d1e69c09e39666722
Benjamin Buccianti <benjamin.buccianti@nubank.com.br>
2024-05-31 21:39:02 -0300

    mark scores

--- src/app/state.cljs
+++ src/app/state.cljs
@@ -3,17 +3,28 @@
             [uix.core :as uix :refer [defui $]]))
 
 (def initial-state {:score {:ok 0 :total 0}
+                    :attempts 0
                     :tones {:a (ai/random-tone)
                             :b (ai/random-tone)}})
 
 (def state-ctx (uix/create-context nil))
 (def dispatch-ctx (uix/create-context nil))
 
+(defn inc-ok-score-if-attempts-are-zero [state]
+  (let [{:keys [attempts]} state]
+    (if (= attempts 0)
+      (update-in state [:score :ok] inc)
+      (assoc state :attempts 0))))
+
 (defn reducer [state {:keys [type]}]
   (case type
+    :error
+    (-> state (update :attempts inc))
+
     :randomize!
     (-> state
         (assoc :tones {:a (ai/random-tone) :b (ai/random-tone)})
+        inc-ok-score-if-attempts-are-zero
         (update-in [:score :total] inc))
 
     state))
