715e45423e9fa40e85c19cba5f041bb80aa562e0
Benjamin Buccianti <bbuccianti@proton.me>
2024-07-18 19:27:25 -0300

    lua deps on build.yml

--- irc.lua
+++ irc.lua
@@ -1,6 +1,7 @@
 local server_port = "irc.libera.chat 6667"
 local channel = "#buccianti"
 local url = os.getenv("JOB_URL") or "???"
+local result = os.getenv("RESULT")
 
 local origin_job_prefix = 'https://builds.sr.ht/bbuccianti/job/'
 local is_origin = url:sub(1, #origin_job_prefix) == origin_job_prefix
@@ -14,10 +15,10 @@ local log = git_log:read("*a"):gsub("\n", " "):gsub("\n", " ")
 
 local nc = io.popen(string.format("nc %s > /dev/null", server_port), "w")
 
-nc:write("NICK tones-build\n")
-nc:write("USER tones-build 8 x : tones-build\n")
+nc:write("NICK tones\n")
+nc:write("USER tones 8 x : tones\n")
 nc:write("JOIN " .. channel .. "\n")
-nc:write(string.format("PRIVMSG %s :Build failure! %s / %s\n",
-                       channel, log, url))
+nc:write(string.format("PRIVMSG :%s Build %s! %s / %s\n",
+                       channel, result, log, url))
 nc:write("QUIT\n")
 nc:close()
